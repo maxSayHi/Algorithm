187da10d94daa66113ea854673a984974fc32654
unknown <>
2019-04-27 17:21:12 +0800

    学习枚举

---
+++ AlgorithmJava/src/bobo/learn/algorithm/primary/LearnEnum.java
@@ -0,0 +1,10 @@
+package bobo.learn.algorithm.primary;
+
+/**
+ * Created by max on 2019/4/27.
+ */
+public class LearnEnum {
+
+    public void testEnum(){
+    }
+}
---
+++ AlgorithmJava/src/bobo/learn/algorithm/primary/MyEnum.java
@@ -0,0 +1,12 @@
+package bobo.learn.algorithm.primary;
+
+/**
+ * Created by max on 2019/4/27.
+ */
+public enum MyEnum{
+    Mon("星期一"),Tuse("星二");
+
+    MyEnum(String name){
+    }
+
+}
